2aec6a41dfb64a8aee05a608c332233b5e277d32
ianblakesleet <ianblakesleet@gmail.com>
2022-04-28 15:25:17 -0600

    3.0

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const path = require('path')
 const Rollbar = require('rollbar')
+const app = express()
 
 let rollbar = new Rollbar({
   accessToken: 'ca08aea249b94f0bbd58580542f50f15',
   captureUncaught: true,
   captureUnhandledRejections: true,
 })
-
-const app = express()
+rollbar.log('Hello world!')
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'))
