788d140ae732cdfeeae801a41b6a7d34cc44110b
ianblakesleet <ianblakesleet@gmail.com>
2022-04-28 14:58:19 -0600

    2.0

--- server/index.js
+++ server/index.js
@@ -1,23 +1,19 @@
 const express = require('express')
 const path = require('path')
+const Rollbar = require('rollbar')
 
-const app = express()
-// include and initialize the rollbar library with your access token
-var Rollbar = require('rollbar')
-var rollbar = new Rollbar({
+let rollbar = new Rollbar({
   accessToken: 'ca08aea249b94f0bbd58580542f50f15',
   captureUncaught: true,
   captureUnhandledRejections: true,
 })
 
-// record a generic message and send it to Rollbar
-rollbar.log('Hello world!')
+const app = express()
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'))
 })
 
 const port = process.env.PORT || 4545
-app.listen(port, () => console.log(`take us to warp ${port}`))
 
-// include and initialize the rollbar library with your access token
+app.listen(port, () => console.log(`Take us to warp ${port}!`))
