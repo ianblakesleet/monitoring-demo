a2abc3848782c9a8ef2277ce4b161577ce5b34c0
ianblakesleet <ianblakesleet@gmail.com>
2022-04-28 16:22:04 -0600

    5.0

--- server/index.js
+++ server/index.js
@@ -9,12 +9,26 @@ let rollbar = new Rollbar({
   captureUnhandledRejections: true,
 })
 rollbar.log('Hello world!')
+let students = []
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'))
   rollbar.info('file served')
 })
+app.post('/api/student', (req, res) => {
+  let { name } = req.body
+  name = name.trim()
+
+  students.push(name)
+
+  rollbar.log('Student added successfully', {
+    author: 'Ian',
+    type: 'manual entry',
+  })
+
+  res.status(200).send(students)
+})
 
 const port = process.env.PORT || 4545
-
+app.use(rollbar.errorHandler())
 app.listen(port, () => console.log(`Take us to warp ${port}!`))
